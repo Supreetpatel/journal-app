232847a020bd420a8c7b84db86177122f01b6752
Supreet0 <supreetpatel2004@gmail.com>
2025-05-11 00:02:20 +0530

    Landing page added with clerk authentication with neon database

---
+++ actions/public.js
@@ -0,0 +1,23 @@
+import { unstable_cache } from "next/cache";
+
+export const getDailyPrompts = unstable_cache(
+  async () => {
+    try {
+      const res = await fetch("https://api.adviceslip.com/advice", {
+        cache: "no-store",
+      });
+      const data = await res.json();
+      return data.slip.advice;
+    } catch (error) {
+      return {
+        success: false,
+        message: "What's on your mind?",
+      };
+    }
+  },
+  ["daily-prompt"],
+  {
+    revalidate: 86400,
+    tags: ["daily-prompt"],
+  }
+);
